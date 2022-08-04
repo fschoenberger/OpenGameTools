aa941bc97a4e800bfe8f03a732c52ad3e5539f34
Frederic Schönberger <24734883+fschoenberger@users.noreply.github.com>
2022-08-04 21:14:50 +0200

    Pulled id, friendly name and path up in IDocumentViewModel.

--- OpenGameTools.Gui/ViewModel/IDocumentViewModel.cs
+++ OpenGameTools.Gui/ViewModel/IDocumentViewModel.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OpenGameTools.Gui.ViewModel
-{
-    public interface IDocumentViewModel
-    {
+namespace OpenGameTools.Gui.ViewModel {
+    public interface IDocumentViewModel {
+        Guid Id { get; }
+
+        string FriendlyName { get; }
+
+        string Path { get; }
     }
 }
