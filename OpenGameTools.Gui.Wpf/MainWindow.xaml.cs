e1636ea87112433766084014587919d5f1e74177
Frederic Schönberger <24734883+fschoenberger@users.noreply.github.com>
2022-08-04 18:54:28 +0200

    Added ReactiveWindow.

--- OpenGameTools.Gui.Wpf/MainWindow.xaml.cs
+++ OpenGameTools.Gui.Wpf/MainWindow.xaml.cs
@@ -8,20 +8,20 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using OpenGameTools.Gui.ViewModel;
+using ReactiveUI;
 
-namespace OpenGameTools.Gui.Wpf
-{
+namespace OpenGameTools.Gui.Wpf {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : ReactiveWindow<IMainWindowViewModel>, IComponentConnector {
+        public MainWindow() {
             InitializeComponent();
         }
     }
---
+++ OpenGameTools.Gui.Wpf/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using OpenGameTools.Gui.ViewModel;
+using ReactiveUI;
+
+namespace OpenGameTools.Gui.Wpf.ViewModel {
+    internal class MainWindowViewModel : ReactiveObject, IMainWindowViewModel {
+        private readonly ILogger<MainWindowViewModel> _logger;
+
+        public MainWindowViewModel(ILogger<MainWindowViewModel> logger) {
+            _logger = logger;
+        }
+    }
+}
