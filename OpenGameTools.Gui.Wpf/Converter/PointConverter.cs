2ae8b571c79ea5f7ef74f3b0707f0126d08a7650
Frederic Schönberger <24734883+fschoenberger@users.noreply.github.com>
2022-09-14 21:11:20 +0200

    Added converter that converts System.Drawing.Point and System.Windows.Point

---
+++ OpenGameTools.Gui.Wpf/Converter/PointConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace OpenGameTools.Gui.Wpf.Converter {
+    public class PointConverter : System.Windows.Data.IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            System.Drawing.Point dp = (System.Drawing.Point)value;
+            return new System.Windows.Point(dp.X, dp.Y);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            System.Windows.Point wp = (System.Windows.Point) value;
+            return new System.Drawing.Point((int) wp.X, (int) wp.Y);
+        }
+    }
+}
