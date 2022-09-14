0322cfe48e7a44daf813badffc6f8f765986a6d9
Frederic Schönberger <24734883+fschoenberger@users.noreply.github.com>
2022-09-14 22:28:43 +0200

    Moved CreateTransition Command Binding to code behind.

--- OpenGameTools.Gui.Wpf/View/QuestDocument/QuestDocument.xaml.cs
+++ OpenGameTools.Gui.Wpf/View/QuestDocument/QuestDocument.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Reactive.Disposables;
 using System.Windows.Markup;
 using OpenGameTools.Gui.ViewModel.Quest;
-using OpenGameTools.Gui.Wpf.ViewModel.DummyDocument;
 using ReactiveUI;
 
 namespace OpenGameTools.Gui.Wpf.View.QuestDocument {
@@ -13,11 +13,11 @@ namespace OpenGameTools.Gui.Wpf.View.QuestDocument {
             InitializeComponent();
 
             this.WhenActivated(d => {
-                d(this.OneWayBind(ViewModel, vm => vm.States, view => view.Editor.ItemsSource));
-                d(this.OneWayBind(ViewModel, vm => vm.Transitions, view => view.Editor.Connections));
+                this.OneWayBind(ViewModel, vm => vm.States, view => view.Editor.ItemsSource).DisposeWith(d);
+                this.OneWayBind(ViewModel, vm => vm.Transitions, view => view.Editor.Connections).DisposeWith(d);
 
-                //d(this.BindCommand(ViewModel, vm => vm.CreateTransition,
-                //    view => view.Editor, nameof(Editor.ConnectionStartedCommand)));
+                this.OneWayBind(ViewModel, vm => vm.CreateTransition, view => view.Editor.ConnectionCompletedCommand)
+                    .DisposeWith(d);
             });
         }
     }
